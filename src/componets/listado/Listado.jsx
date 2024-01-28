6ee4b9235160f5f56a392856450a05234c09c72c
eneatei <nataliaochoa@MacBook-Pro-de-Natalia.local>
2024-01-27 21:43:31 -0300

    solo falta visualizar el agregado en la tabla

--- src/componets/listado/Listado.jsx
+++ src/componets/listado/Listado.jsx
@@ -1,33 +1,32 @@
-import { Table } from "react-bootstrap"
+import { Table } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export const Listado = ({ colaboradores, setColaboradores, filterColaboradores, setFilterColaboradores }) => {
+  
+  const borrarColaborador = (id) => {
+    const nuevosColaboradores = colaboradores.filter((colaborador) => colaborador.id !== id);
+    setColaboradores(nuevosColaboradores);
+    setFilterColaboradores(nuevosColaboradores);
+  }
 
+  const renderTablaColaboradores = () => {
+    return filterColaboradores.map((colaborador) => (
+      <tr key={colaborador.id}>
+        <td>{colaborador.nombre}</td>
+        <td>{colaborador.correo}</td>
+        <td>{colaborador.edad}</td>
+        <td>{colaborador.cargo}</td>
+        <td>{colaborador.telefono}</td>
+        <td>
+          <FontAwesomeIcon icon={faTrash} onClick={() => borrarColaborador(colaborador.id)} />
+        </td>
+      </tr>
+    ));
+  };
 
-const borrarColaborador = (id) => {
-  const nuevosColaboradores = colaboradores.filter((colaborador) => colaborador.id !== id);
-  setColaboradores(nuevosColaboradores);
-  setFilterColaboradores(nuevosColaboradores);
-}
-
-const renderTablaColaboradores = () => {
-  return filterColaboradores.map((colaborador) => (
-    <tr key={colaborador.id}>
-      <td>{colaborador.nombre}</td>
-      <td>{colaborador.correo}</td>
-      <td>{colaborador.edad}</td>
-      <td>{colaborador.cargo}</td>
-      <td>{colaborador.telefono}</td>
-      <td>
-        <i className="fa-solid fa-trash-can"
-          onClick={() => borrarColaborador(colaborador.id)} ></i>
-      </td>
-    </tr>
-  ));
-};
-
-
-return (
-<Table responsive striped bordered hover>
+  return (
+    <Table responsive striped bordered hover>
       <thead>
         <tr>
           <th>Nombre</th>
@@ -35,12 +34,12 @@ return (
           <th>Edad</th>
           <th>Cargo</th>
           <th>Teléfono</th>
-        <th>Eliminar</th>
+          <th>Eliminar</th>
         </tr>
       </thead>
       <tbody>
         {renderTablaColaboradores()}
-        </tbody>
+      </tbody>
     </Table>
-  )
+  );
 };
